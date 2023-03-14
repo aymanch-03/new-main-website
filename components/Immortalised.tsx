9e38c731f95ae773dff586b365f3d70d6706b25a
Aymen <Aymen@MacBook-Air.local>
2023-03-14 22:35:32 +0100

    change app name

--- components/Immortalised.tsx
+++ components/Immortalised.tsx
@@ -24,7 +24,7 @@ const Immortalised = () => {
     "/Website Images/Page 6 - Immortalised/7.png",
     "/Website Images/Page 6 - Immortalised/8.png",
   ];
-  function shuffleArray(array) {
+  function shuffleArray(array: any[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
